1c1aee5f2e70813762dcaccf555a014c98a9dbcc
wweir <wweir@foxmail.com>
2023-06-28 20:46:35 +0800

    feat: add mock listener

---
+++ mocklistener/listener.go
@@ -0,0 +1,24 @@
+package mocklistener
+
+import "net"
+
+var _ net.Listener = (*Listener)(nil)
+
+type Listener struct {
+	// Address is the address of the listener
+	Address net.Addr
+	// ConnCh must be closed after Close manually
+	ConnCh chan net.Conn
+}
+
+func (ln *Listener) Accept() (net.Conn, error) {
+	return <-ln.ConnCh, nil
+}
+
+func (ln *Listener) Close() error {
+	return nil
+}
+
+func (ln *Listener) Addr() net.Addr {
+	return ln.Address
+}
